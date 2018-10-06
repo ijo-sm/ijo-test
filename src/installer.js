327da53e5aef35fb64f39fc18ea5c8acde371615
JortvD <jortvandriel@hotmail.com>
2018-10-06 10:18:33 +0200

    Added the Installer class

---
+++ src/installer.js
@@ -0,0 +1,43 @@
+const Utils = require("./utils");
+
+class Installer {
+	async ijo(config) {
+		if(typeof config.ijo !== "object") {
+			config.ijo = {
+				type: "git",
+				path: "https://github.com/ijo-sm/ijo.git",
+				branch: "dev",
+				ignore_self: false
+			}
+		}
+
+		if(Utils.getExactPackageName() === "ijo" && !config.ijo.ignore_self) {
+			config.ijo.type = "fs";
+			config.ijo.path = Utils.path();
+		}
+
+		await Utils.install(config.ijo, Utils.path() + "/test/panel");
+	}
+
+	async plugin(config, name, location) {
+		if(Utils.getExactPackageName() === name && !config.ignore_self) {
+			config.type = "fs";
+			config.path = Utils.path();
+		}
+
+		await Utils.install(config, Utils.path() + "/test/" + location + "/plugins/" + name);
+	}
+
+	async executor(config, name, location) {
+		if(Utils.getExactPackageName() === name && !config.ignore_self) {
+			config.type = "fs";
+			config.path = Utils.path();
+		}
+
+		Utils.mkdir(Utils.path() + "/test/" + location + "/executors/" + name);
+
+		await Utils.install(config, Utils.path() + "/test/" + location + "/executors/" + name);
+	}
+}
+
+module.exports = new Installer();
