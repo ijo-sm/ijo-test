4d9bde929ee6719b7416a82ecc22cee4a3a022d2
JortvD <jortvandriel@hotmail.com>
2018-10-14 18:34:22 +0200

    Added a machine folder class

---
+++ src/folders/machine.js
@@ -0,0 +1,25 @@
+const Installer = require("./../installer");
+
+module.exports = class PanelFolder {
+	constructor(config) {
+		this.config = config;
+	}
+
+	async create() {
+		this.createFolder();
+		await this.installMachine();
+	}
+
+	createFolder() {
+		Utils.fs.mkdir(Utils.path.get() + "/test/machine");
+	}
+
+	async installMachine() {
+		Logger.info("machine", "installing");
+		Logger.profile("machine");
+
+		await Installer.machine(this.config);
+
+		Logger.profile("machine", "info", "installing finished");
+	}
+}
