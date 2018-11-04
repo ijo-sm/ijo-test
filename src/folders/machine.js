8975e2f1c769e923ab54500d46abc6e2df1cfeaa
JortvD <jortvandriel@hotmail.com>
2018-11-04 11:29:55 +0100

    Let the machine test also create a plugins folder

--- src/folders/machine.js
+++ src/folders/machine.js
@@ -1,6 +1,7 @@
 const Installer = require("./../installer");
+const PluginsFolder = require("./plugins");
 
-module.exports = class PanelFolder {
+module.exports = class MachineFolder {
 	constructor(config) {
 		this.config = config;
 	}
@@ -8,12 +9,22 @@ module.exports = class PanelFolder {
 	async create() {
 		this.createFolder();
 		
+		await this.createFolders();
 		await this.installMachine();
 		await this.installPackages();
 	}
 
 	createFolder() {
 		Utils.fs.mkdir(`${Utils.path.get()}/test/machine`);
+	}
+
+	async createFolders() {
+		Logger.info("PluginsFolder", "creating");
+		Logger.profile("PluginsFolder");
+
+		await new PluginsFolder(this.config, "machine").create();
+
+		Logger.profile("PluginsFolder", "info", "creating finished");
 	}
 
 	async installMachine() {
